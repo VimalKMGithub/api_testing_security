340d426f502a935fef8cdb4732da3d301df0a374
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-05 13:35:12 +0530

    .

---
+++ src/test/java/org/vimal/helpers/CleanUpHelper.java
@@ -0,0 +1,56 @@
+package org.vimal.helpers;
+
+import org.vimal.dtos.UserDto;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+import static org.vimal.api.AdminCallsUsingGlobalAdminUser.deleteUsers;
+import static org.vimal.constants.Common.ENABLE;
+import static org.vimal.constants.Common.MAX_BATCH_SIZE_OF_USER_DELETION_AT_A_TIME;
+
+public final class CleanUpHelper {
+    private CleanUpHelper() {
+    }
+
+    public static void cleanUpTestUsers(Object... inputs) {
+        Set<String> identifiers = extractUserIdentifiers(inputs);
+        Iterator<String> iterator = identifiers.iterator();
+        Set<String> batch = new HashSet<>();
+        while (iterator.hasNext()) {
+            batch.clear();
+            while (iterator.hasNext() && batch.size() < MAX_BATCH_SIZE_OF_USER_DELETION_AT_A_TIME) {
+                batch.add(iterator.next());
+            }
+            try {
+                deleteUsers(batch, ENABLE, ENABLE);
+            } catch (Exception ignored) {
+            }
+        }
+    }
+
+    private static Set<String> extractUserIdentifiers(Object... inputs) {
+        Set<String> result = new HashSet<>();
+        for (Object input : inputs) {
+            switch (input) {
+                case null -> {
+                }
+                case String str -> result.add(str);
+                case UserDto dto -> result.add(dto.getUsername());
+                case Iterable<?> iterable -> {
+                    for (Object element : iterable) {
+                        result.addAll(extractUserIdentifiers(element));
+                    }
+                }
+                case Object[] objects -> {
+                    for (Object element : objects) {
+                        result.addAll(extractUserIdentifiers(element));
+                    }
+                }
+                default -> throw new RuntimeException("Unsupported input type: " + input.getClass());
+            }
+        }
+        return result;
+    }
+}
