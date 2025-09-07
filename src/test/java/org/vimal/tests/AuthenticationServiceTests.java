e0bbe410adc917aa35dbad38c7d2f712408b120d
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-07 23:05:28 +0530

    .

--- src/test/java/org/vimal/tests/AuthenticationServiceTests.java
+++ src/test/java/org/vimal/tests/AuthenticationServiceTests.java
@@ -7,6 +7,7 @@ import org.vimal.dtos.UserDto;
 
 import static org.hamcrest.Matchers.*;
 import static org.vimal.api.AuthenticationCalls.login;
+import static org.vimal.helpers.InvalidInputsHelper.*;
 
 public class AuthenticationServiceTests extends BaseTest {
     @Test
@@ -18,5 +19,31 @@ public class AuthenticationServiceTests extends BaseTest {
                 .body("refresh_token", notNullValue())
                 .body("expires_in_seconds", equalTo(1800))
                 .body("token_type", containsString("Bearer"));
+    }
+
+    @Test
+    public void test_Login_Failure_InvalidCredentials() {
+        Response response = login("invalidUser", "wrongPassword@1");
+        response.then().statusCode(401)
+                .body("error", containsString("Unauthorized"))
+                .body("message", containsString("Invalid credentials"));
+        for (String invalidUsername : INVALID_USERNAMES) {
+            response = login(invalidUsername, "SomePassword@1");
+            response.then().statusCode(401)
+                    .body("error", containsString("Unauthorized"))
+                    .body("message", containsString("Invalid credentials"));
+        }
+        for (String invalidEmail : INVALID_EMAILS) {
+            response = login(invalidEmail, "SomePassword@1");
+            response.then().statusCode(401)
+                    .body("error", containsString("Unauthorized"))
+                    .body("message", containsString("Invalid credentials"));
+        }
+        for (String invalidPassword : INVALID_PASSWORDS) {
+            response = login("SomeUser", invalidPassword);
+            response.then().statusCode(401)
+                    .body("error", containsString("Unauthorized"))
+                    .body("message", containsString("Invalid credentials"));
+        }
     }
 }
