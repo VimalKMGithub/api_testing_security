7b2960be5d0677639bb087c355a63debfb20ff85
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-07 23:10:57 +0530

    .

--- src/test/java/org/vimal/tests/AuthenticationServiceTests.java
+++ src/test/java/org/vimal/tests/AuthenticationServiceTests.java
@@ -18,32 +18,47 @@ public class AuthenticationServiceTests extends BaseTest {
                 .body("access_token", notNullValue())
                 .body("refresh_token", notNullValue())
                 .body("expires_in_seconds", equalTo(1800))
-                .body("token_type", containsString("Bearer"));
+                .body("token_type", containsStringIgnoringCase("Bearer"));
     }
 
     @Test
     public void test_Login_Failure_InvalidCredentials() {
         Response response = login("invalidUser", "wrongPassword@1");
         response.then().statusCode(401)
-                .body("error", containsString("Unauthorized"))
-                .body("message", containsString("Invalid credentials"));
+                .body("error", containsStringIgnoringCase("Unauthorized"))
+                .body("message", containsStringIgnoringCase("Invalid credentials"));
         for (String invalidUsername : INVALID_USERNAMES) {
             response = login(invalidUsername, "SomePassword@1");
             response.then().statusCode(401)
-                    .body("error", containsString("Unauthorized"))
-                    .body("message", containsString("Invalid credentials"));
+                    .body("error", containsStringIgnoringCase("Unauthorized"))
+                    .body("message", containsStringIgnoringCase("Invalid credentials"));
         }
         for (String invalidEmail : INVALID_EMAILS) {
             response = login(invalidEmail, "SomePassword@1");
             response.then().statusCode(401)
-                    .body("error", containsString("Unauthorized"))
-                    .body("message", containsString("Invalid credentials"));
+                    .body("error", containsStringIgnoringCase("Unauthorized"))
+                    .body("message", containsStringIgnoringCase("Invalid credentials"));
         }
         for (String invalidPassword : INVALID_PASSWORDS) {
             response = login("SomeUser", invalidPassword);
             response.then().statusCode(401)
-                    .body("error", containsString("Unauthorized"))
-                    .body("message", containsString("Invalid credentials"));
+                    .body("error", containsStringIgnoringCase("Unauthorized"))
+                    .body("message", containsStringIgnoringCase("Invalid credentials"));
         }
+    }
+
+    @Test
+    public void test_Account_Lockout_After_Multiple_Failed_Login_Attempts() {
+        UserDto user = createTestUser();
+        for (int i = 0; i < 5; i++) {
+            Response response = login(user.getUsername(), "WrongPassword@1");
+            response.then().statusCode(401)
+                    .body("error", containsStringIgnoringCase("Unauthorized"))
+                    .body("message", containsStringIgnoringCase("Invalid credentials"));
+        }
+        Response response = login(user.getUsername(), "WrongPassword@1");
+        response.then().statusCode(401)
+                .body("error", containsStringIgnoringCase("Unauthorized"))
+                .body("message", containsStringIgnoringCase("Account locked1"));
     }
 }
