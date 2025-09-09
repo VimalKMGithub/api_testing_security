04d3ec9740329143ad3d2ba7ba1e7f19e55eae1f
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-09 20:16:17 +0530

    .

--- src/test/java/org/vimal/constants/Common.java
+++ src/test/java/org/vimal/constants/Common.java
@@ -9,6 +9,9 @@ public final class Common {
     public static final String ENABLE = "enable";
     public static final String DISABLE = "disable";
     public static final String HARD = "hard";
+    public static final String FORCE = "force";
+    public static final String EMAIL_MFA = "EMAIL_MFA";
+    public static final String AUTHENTICATOR_APP_MFA = "AUTHENTICATOR_APP_MFA";
     public static final String LENIENCY = "leniency";
     public static final int MAX_BATCH_SIZE_OF_USER_CREATION_AT_A_TIME = 34;
     public static final int MAX_BATCH_SIZE_OF_USER_DELETION_AT_A_TIME = 34;
