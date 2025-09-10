f9ce5d26e24c3e7cee153e5fdb27d17192d0e9bf
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-10 09:34:48 +0530

    .

--- src/test/java/org/vimal/api/UserCalls.java
+++ src/test/java/org/vimal/api/UserCalls.java
@@ -3,20 +3,26 @@ package org.vimal.api;
 import io.restassured.response.Response;
 import org.vimal.dtos.UserDto;
 
+import java.util.concurrent.ExecutionException;
+
+import static org.vimal.api.ApiCalls.executeRequest;
+import static org.vimal.api.Common.waitForResponse;
 import static org.vimal.constants.SubPaths.USER;
+import static org.vimal.enums.RequestMethods.POST;
 
 public final class UserCalls {
     private UserCalls() {
     }
 
-    public static Response register(UserDto user) {
-        return ApiCalls.executeRequest(
-                org.vimal.enums.RequestMethods.POST,
-                USER + "/register",
-                null,
-                null,
-                null,
-                user
+    public static Response register(UserDto user) throws ExecutionException, InterruptedException {
+        return waitForResponse(() -> executeRequest(
+                        POST,
+                        USER + "/register",
+                        null,
+                        null,
+                        null,
+                        user
+                )
         );
     }
 }
