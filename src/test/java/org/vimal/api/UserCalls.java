70cc62fd829af22abcf18b36d7aab5280d2a11ca
Vimal Kumar Mishra <vimalkumarm472@gmail.com>
2025-09-10 11:46:46 +0530

    .

--- src/test/java/org/vimal/api/UserCalls.java
+++ src/test/java/org/vimal/api/UserCalls.java
@@ -3,11 +3,15 @@ package org.vimal.api;
 import io.restassured.response.Response;
 import org.vimal.dtos.UserDto;
 
+import java.util.Map;
 import java.util.concurrent.ExecutionException;
 
 import static org.vimal.api.ApiCalls.executeRequest;
 import static org.vimal.api.Common.waitForResponse;
+import static org.vimal.constants.Common.AUTHORIZATION;
+import static org.vimal.constants.Common.BEARER;
 import static org.vimal.constants.SubPaths.USER;
+import static org.vimal.enums.RequestMethods.GET;
 import static org.vimal.enums.RequestMethods.POST;
 
 public final class UserCalls {
@@ -22,6 +26,15 @@ public final class UserCalls {
                         null,
                         null,
                         user
+                )
+        );
+    }
+
+    public static Response getSelfDetails(String accessToken) throws ExecutionException, InterruptedException {
+        return waitForResponse(() -> executeRequest(
+                        GET,
+                        USER + "/getSelfDetails",
+                        Map.of(AUTHORIZATION, BEARER + accessToken)
                 )
         );
     }
--- src/test/java/org/vimal/tests/UserServiceTests.java
+++ src/test/java/org/vimal/tests/UserServiceTests.java
@@ -8,6 +8,8 @@ import org.vimal.dtos.UserDto;
 import java.util.concurrent.ExecutionException;
 
 import static org.hamcrest.Matchers.*;
+import static org.vimal.api.AuthenticationCalls.getAccessToken;
+import static org.vimal.api.UserCalls.getSelfDetails;
 import static org.vimal.api.UserCalls.register;
 import static org.vimal.helpers.DtosHelper.createRandomUserDto;
 
@@ -27,5 +29,23 @@ public class UserServiceTests extends BaseTest {
                 .body("user.middleName", equalTo(user.getMiddleName()))
                 .body("user.lastName", equalTo(user.getLastName()))
                 .body("user.createdBy", containsStringIgnoringCase("SELF"));
+    }
+
+    @Test
+    public void test_Get_Self_Details_Success() throws ExecutionException, InterruptedException {
+        UserDto user = createTestUser();
+        Response response = getSelfDetails(getAccessToken(
+                        user.getUsername(),
+                        user.getPassword()
+                )
+        );
+        response.then()
+                .statusCode(200)
+                .body("id", notNullValue())
+                .body("username", equalTo(user.getUsername()))
+                .body("email", equalTo(user.getEmail()))
+                .body("firstName", equalTo(user.getFirstName()))
+                .body("middleName", equalTo(user.getMiddleName()))
+                .body("lastName", equalTo(user.getLastName()));
     }
 }
